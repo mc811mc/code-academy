42375546af9fcd183cc37226fc8ef611a2fe4e5f
Minho Cho <mc811mc@gmail.com>
2024-09-21 21:11:33 -0400

    Create initials.py

---
+++ initials.py
@@ -0,0 +1,24 @@
+# Minho Cho
+# I translate novels
+first_initial = """
+M       M
+M M   M M
+M M   M M
+M   M   M
+M       M
+M       M
+M       M
+"""
+
+second_initial = """
+  C C C
+C       C
+C
+C
+C
+C       C
+  C C C
+"""
+
+print(first_initial)
+print(second_initial)
